1955481a6b35dfc912b94228ea4ef388987bbf7a
julio177 <julioc.aguilarv@gmail.com>
2017-08-06 15:33:20 -0500

    Begin cpp version replace spaces problem

---
+++ Problems/ArraysAndStrings/ReplaceSpaces/main.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+#define INF 1000000000
+#define FOR(i, a, b) for(int i=int(a); i<int(b); i++)
+#define FORC(cont, it) for(decltype((cont).begin()) it = (cont).begin(); it != (cont).end(); it++)
+#define pb push_back
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef stack<int> si;
+typedef stack<string> ss;
+typedef vector<ii> vii;
+typedef vector<vi> vvi;
+
+string repalceSpaces(string str) {
+    
+}
+
+int main() {
+    int cases;
+    string str;
+    cin >> cases;
+    FOR(i, 0, cases) {
+        cin >> str;
+        cout << repalceSpaces(str) << endl;
+    }
+}
