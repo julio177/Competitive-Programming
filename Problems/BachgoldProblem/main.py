ac92de47983896941159c5a049a9c7af4af8dcda
julio177 <julioc.aguilarv@gmail.com>
2017-09-25 11:31:13 -0500

    AC bachlog

---
+++ Problems/BachgoldProblem/main.py
@@ -0,0 +1,15 @@
+def main():
+    num = int(input())
+    first = int(num / 2) - 1
+    nums = [2] * first
+    resi = num - (first * 2)
+    if resi - 2 > 0:
+        nums.append(3)
+    else:
+        nums.append(2)
+    print(len(nums))
+    for i in nums:
+        print(i, end=" ")
+
+
+main()
