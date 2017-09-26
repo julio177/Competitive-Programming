c0aa88fe810ef714ee6db20075d67e5ec8b53b9a
julio177 <julioc.aguilarv@gmail.com>
2017-09-26 13:31:17 -0500

    AC set of strings

--- Problems/SetOfStrings/main.py
+++ Problems/SetOfStrings/main.py
@@ -5,19 +5,45 @@ def main():
     if n > len(s):
         print('NO')
         return
-    else:
-        part = int(len(s) / n)
-        initial = 0
+    elif n == 1:
         print('YES')
+        print(s)
+    else:
         strings = []
-        for i in range(0, n):
-            strings.append(s[initial:part])
-            initial = part
-            part += part
-        resi = len(s) % n
-        if resi > 0:
-            strings[-1] = strings[-1] + s[-resi:]
+        letters = len(set(list(s)))
+        if letters < n:
+            print('NO')
+            return
+        curr = s[0]
+        comp = s[0]
+        used = {s[0]: 1}
+        cant = 0
+        for i in range(1, len(s)):
+            if s[i] == comp:
+                curr += s[i]
+            else:
+                check = used.get(s[i], -1)
+                if check != -1:
+                    curr += s[i]
+                    if i == len(s) - 1:
+                        strings.append(curr)
+                        break
+                    continue
+                else:
+                    strings.append(curr)
+                    cant += 1
+                    if cant == n:
+                        strings[-1] = strings[-1] + s[i:]
+                        break
+                    curr = s[i]
+                    comp = s[i]
+                    used[comp] = 1
+                    if i == len(s) - 1:
+                        strings.append(curr)
+                        break
+        if curr != strings[-1] and cant != n:
+            strings.append(curr)
+        print('YES')
         for i in strings:
             print(i)
-
 main()
