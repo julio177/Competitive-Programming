389cd9477ca547c39b2312d837baae041c8dad23
julio177 <julioc.aguilarv@gmail.com>
2017-09-27 13:33:22 -0500

    AC on leha and functions

---
+++ Problems/TravelCard/main.py
@@ -0,0 +1,10 @@
+# http://codeforces.com/problemset/problem/756/B
+def main():
+    n = int(input())
+    acum = 0
+    max_time = 0
+    for i in range(n):
+        num = int(input())
+        
+
+main()
